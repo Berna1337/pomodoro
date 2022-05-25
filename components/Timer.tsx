58bf5b9ae1f29feff40d6451fe6c61e8cd8f18e6
Bernardo Cruz <bernaabeu@outlook.pt>
2022-05-25 12:30:26 +0100

    Added the timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 export default function Timer() {
-	return <div>timer</div>;
+	const children = ({ remainingTime }: any) => {
+		const minutes = Math.floor(remainingTime / 60);
+		const seconds = String(remainingTime % 60).padStart(2, "0");
+
+		return `${minutes}:${seconds}`;
+	};
+
+	return (
+		<CountdownCircleTimer
+			isPlaying
+			duration={1500}
+			colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+			colorsTime={[7, 5, 2, 0]}
+		>
+			{children}
+		</CountdownCircleTimer>
+	);
 }
